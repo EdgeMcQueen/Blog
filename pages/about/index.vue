c4069d813081a04ca332792ed430a0520fabb493
Sardor.Mutalov <sardor.mutalov@gmail.com>
2020-12-09 06:26:00 +0300

    about page

---
+++ pages/about/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="about">
+      <div class="container">
+        <h1 class="title">About my blog:</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsa
+          facilis quae adipisci fugiat amet necessitatibus cum, officiis veritatis in
+          sapiente. Tenetur, mollitia ad temporibus fugiat ipsam cupiditate porro
+          consequuntur?
+        </p>
+        <Intro title="Lorem ipsum dolor sit amet" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsa
+          facilis quae adipisci fugiat amet necessitatibus cum, officiis veritatis in
+          sapiente. Tenetur, mollitia ad temporibus fugiat ipsam cupiditate porro
+          consequuntur?
+        </p>
+        <img src="@/assets/img/about.jpg" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ipsa
+          facilis quae adipisci fugiat amet necessitatibus cum, officiis veritatis in
+          sapiente. Tenetur, mollitia ad temporibus fugiat ipsam cupiditate porro
+          consequuntur?
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Intro from "@/components/UI/Intro.vue";
+export default {
+  components: {
+    Intro,
+  },
+};
+</script>
+
+<style lang="scss">
+.about {
+  text-align: center;
+  p {
+    margin-bottom: 10px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
