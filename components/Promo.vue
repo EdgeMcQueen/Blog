f0ad820da4723801d0190edb5f1c9b187de46d25
Sardor.Mutalov <sardor.mutalov@gmail.com>
2020-12-10 09:01:31 +0300

    blog main page

---
+++ components/Promo.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="promo">
+    <div class="container">
+      <h1 class="title">My SSR blog! With Nuxt.js</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, voluptatibus distinctio fugiat exercitationem
+        architecto voluptates amet molestias explicabo debitis id assumenda non inventore culpa minus omnis, aliquid
+        nulla reiciendis dolores!
+      </p>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+.promo {
+  text-align: center;
+  p {
+    color: #999999;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
-    <div class="container"></div>
+    <div class="container">
+      <Promo />
+      <Intro title="My last posts:" />
+      <!-- posts -->
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Promo from "@/components/Promo.vue";
+import Intro from "@/components/UI/Intro.vue";
+export default {
+  components: {
+    Intro,
+    Promo,
+  },
+};
 </script>
