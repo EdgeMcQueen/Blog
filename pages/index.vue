1102863ba52ab31f79f705c083505766e8db5339
Sardor.Mutalov <sardor.mutalov@gmail.com>
2020-12-12 08:25:53 +0300

    posts render

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <Promo />
       <Intro title="My last posts:" />
       <!-- posts -->
+      <PostsList :posts="posts" />
     </div>
   </div>
 </template>
@@ -11,10 +12,40 @@
 <script>
 import Promo from "@/components/Promo.vue";
 import Intro from "@/components/UI/Intro.vue";
+import PostsList from "@/components/blog/PostsList.vue";
+
 export default {
   components: {
+    PostsList,
     Intro,
     Promo,
+  },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "1 post",
+          descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          img:
+            "https://images.pexels.com/photos/3690511/pexels-photo-3690511.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 2,
+          title: "2 post",
+          descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          img:
+            "https://images.pexels.com/photos/4666751/pexels-photo-4666751.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+        {
+          id: 3,
+          title: "3 post",
+          descr: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+          img:
+            "https://images.pexels.com/photos/1894350/pexels-photo-1894350.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        },
+      ],
+    };
   },
 };
 </script>
